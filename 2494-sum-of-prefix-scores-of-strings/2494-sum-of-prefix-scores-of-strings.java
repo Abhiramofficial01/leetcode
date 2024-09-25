c3c872fdd75e4327a8e56edc8780b859a579b4a5
Abhiram M <91586067+Abhiramofficial01@users.noreply.github.com>
2024-09-25 09:13:46 +0530

    Time: 31 ms (98.06%), Space: 51.6 MB (96.77%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.java
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.java
@@ -1,60 +1,51 @@
-class Node {
-    Map<Character, Pair> umc = new HashMap<>();
-    boolean isEnd = false;
-}
-class Pair {
-    int count;
-    Node node;
-
-    Pair(int count, Node node) {
-        this.count = count;
-        this.node = node;
-    }
-}
-class Trie {
-    private Node root;
-
-    public Trie() {
-        root = new Node();
-    }
-    public void insert(String s) {
-        Node cur = root;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!cur.umc.containsKey(c)) {
-                cur.umc.put(c, new Pair(0, new Node()));
-            }
-            cur.umc.get(c).count++;
-            cur = cur.umc.get(c).node;
-        }
-        cur.isEnd = true;
-    }
-    public int checkPrefixCount(String s) {
-        Node cur = root;
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!cur.umc.containsKey(c)) {
-                break;
-            }
-            count += cur.umc.get(c).count;
-            cur = cur.umc.get(c).node;
-        }
-        return count;
-    }
-}
-
 class Solution {
     public int[] sumPrefixScores(String[] words) {
-        Trie trie = new Trie();
-        for (String word : words) {
-            trie.insert(word);
+        int wordCount = words.length;
+        Integer[] sortedIndices = new Integer[wordCount];
+        for (int i = 0; i < wordCount; i++) {
+            sortedIndices[i] = i;
         }
-        int[] ans = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            ans[i] = trie.checkPrefixCount(words[i]);
-        }
+        Arrays.sort(sortedIndices, (a, b) -> words[a].compareTo(words[b]));
+        
+        int[] commonPrefixLengths = calculateCommonPrefixLengths(words, sortedIndices);
+        int[] scores = calculateScores(words, sortedIndices, commonPrefixLengths);
+        return scores;
+    }
 
-        return ans;
+    private int[] calculateCommonPrefixLengths(String[] words, Integer[] sortedIndices) {
+        int[] commonPrefixLengths = new int[words.length];
+        for (int i = 1; i < words.length; i++) {
+            String prevWord = words[sortedIndices[i - 1]];
+            String currWord = words[sortedIndices[i]];
+            int commonLength = 0;
+            while (commonLength < prevWord.length() && 
+                   commonLength < currWord.length() && 
+                   prevWord.charAt(commonLength) == currWord.charAt(commonLength)) {
+                commonLength++;
+            }
+            commonPrefixLengths[i] = commonLength;
+        }
+        return commonPrefixLengths;
+    }
+
+    private int[] calculateScores(String[] words, Integer[] sortedIndices, int[] commonPrefixLengths) {
+        int[] scores = new int[words.length];
+        for (int i = 0; i < sortedIndices.length; i++) {
+            int wordIndex = sortedIndices[i];
+            int wordLength = words[wordIndex].length();
+            scores[wordIndex] += wordLength;
+            int j = i + 1;
+            int commonLength = wordLength;
+            while (j < words.length) {
+                commonLength = Math.min(commonLength, commonPrefixLengths[j]);
+                if (commonLength == 0) {
+                    break;
+                }
+                scores[wordIndex] += commonLength;
+                scores[sortedIndices[j]] += commonLength;
+                j++;
+            }
+        }
+        return scores;
     }
 }
