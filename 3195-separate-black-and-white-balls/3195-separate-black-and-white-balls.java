8f57076cc872d9558d2b803af2827e19c3dcd47b
Abhiram M <91586067+Abhiramofficial01@users.noreply.github.com>
2024-10-15 11:18:39 +0530

    Time: 7 ms (100%), Space: 45.5 MB (30.47%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long swap=0;
+        int b=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                swap+=(int)b;
+            }
+            else{
+                b++;
+            }
+        }
+        return swap;
+    }
+}
